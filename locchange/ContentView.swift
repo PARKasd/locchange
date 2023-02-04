802adbdbf827e7f4257f946743586d33d306a300
0won2 <66772247+PARKasd@users.noreply.github.com>
2023-02-05 00:40:35 +0900

    Add files via upload
    info 삭제, 추후 기능추가를 위한 버튼변경

--- locchange/ContentView.swift
+++ locchange/ContentView.swift
@@ -8,45 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var showingActionSheet = false
-    
+    @State private var per: Float = 0.0
     private let dynamicPath = "/var/containers/Shared/SystemGroup/systemgroup.com.apple.mobilegestaltcache/Library/Caches/com.apple.MobileGestalt.plist"
     var body: some View {
         NavigationView{
+            
             VStack{
                 Image("Logo").resizable().frame(width: 150,height: 150)
                 Text("Locchange")
                     .font(.title2)
                     .fontWeight(.bold)
-                
-                Button("시작") {
-                    plistChange(plistPath: dynamicPath, key: "h63QSdBCiT/z0WU6rdQv6Q", value: "US")
-                    
-                    DispatchQueue.main.asyncAfter(deadline: .now() + 10){
-                        plistChange(plistPath: dynamicPath, key: "zHeENZu+wbg7PUprwNwBWg", value: "LL/A")
+                Menu("시작하기"){
+                    Button("시작") {
                         
+                        plistChange(plistPath: dynamicPath, key: "h63QSdBCiT/z0WU6rdQv6Q", value: "US")
+                        per = 0.5
                         DispatchQueue.main.asyncAfter(deadline: .now() + 10){
-                            respring()
-                        }}}.padding()
-            }
-            .navigationBarItems(trailing:
-                Button(action: {
-                    showingActionSheet = true
-                }) {
-                    Image(systemName: "info.circle")
+                            plistChange(plistPath: dynamicPath, key: "zHeENZu+wbg7PUprwNwBWg", value: "LL/A")
+                            per = 0.9
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 10){
+                                per = 1.0
+                                respring()
+                            }}}
+                    Link("지원",destination: URL(string: "https://discord.gg/4CepjXqVzK")!)
+                    
                 }
-                .confirmationDialog("Select",
-                                    isPresented: $showingActionSheet,
-                                    actions: {
-                Link("Developed by parkm04", destination: URL(string: "https://toss.me/parkm04")!)
-                Link("Designed by gamjanakg", destination: URL(string: "https://toss.me/gamjanakg")!)
-                Link("Discord", destination: URL(string: "https://discord.gg/4CepjXqVzK")!)
-                Button("Dismiss", role: .cancel) { }
-            })
-            )
+                ProgressView(value: per).scaleEffect(0.7, anchor: .center)
+                    
+                    .padding()
+            }
+            }
+            
         }
         }
-    }
+    
 
 
 func plistChange(plistPath: String, key: String, value: String) {
@@ -71,16 +66,16 @@ func plistChange(plistPath: String, key: String, value: String) {
     let newData = try! PropertyListSerialization.data(fromPropertyList: newPlist, format: .binary, options: 0)
 
     overwriteFile(newData, plistPath)
+
+    
 }
-
-
 func respring(){
     guard let window = UIApplication.shared.connectedScenes.compactMap({ ($0 as? UIWindowScene)?.keyWindow }).first else { return }
     while true {
        window.snapshotView(afterScreenUpdates: false)
     }
 }
-
+func test() {}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
